ce95e930d524217a2c806567044611ceeae452d3
Nicolas Pucci <nicolaspucci1989@gmail.com>
2023-03-22 01:08:10 -0300

    wip

--- index.js
+++ index.js
@@ -8,51 +8,55 @@ const saltRounds = 10
 const secret = 'secret'
 
 const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-    modulusLength: 2048,
+	modulusLength: 2048,
 });
 const auth = {}
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+	res.send('Hello World!')
 })
 
 app.post('/signup', async (req, res) => {
-    const {user, password} = req.body
-    const hashedPassword = await bcrypt.hash(password, saltRounds)
-    auth[user] = hashedPassword
-    console.log(auth)
-    res.send(hashedPassword)
+	const { user, password } = req.body
+	const hashedPassword = await bcrypt.hash(password, saltRounds)
+	auth[user] = hashedPassword
+	console.log(auth)
+	res.send(hashedPassword)
 })
 
+function encodeToBase64Url(data) {
+	return Buffer.from(JSON.stringify(data)).toString("base64url")
+}
+
 app.post('/signin', async (req, res) => {
-    const {user, password} = req.body
-    const result = await bcrypt.compare(password, auth[user])
-    if (result) {
-        const tokenHeader = {
-            alg: 'RS256',
-            typ: 'JWT'
-        }
-        const tokenPayload = {
-            name: user,
-            role: 'user'
-        }
-        const headerBuffer = Buffer.from(JSON.stringify(tokenHeader))
-        const payloadBuffer = Buffer.from(JSON.stringify(tokenPayload))
-        const signature = crypto.sign('sha256',payloadBuffer, {
-            key: privateKey,
-            padding: crypto.constants.RSA_PKCS1_PADDING
-        })
-        const signatureString = signature.toString('base64')
-        const jwt = `${headerBuffer.toString('base64')}.${payloadBuffer.toString('base64')}.${signatureString}`
-        res.send(JSON.stringify({jwt}))
-    }
-    else {
-        res.send(500)
-    }
+	const { user, password } = req.body
+	const result = await bcrypt.compare(password, auth[user])
+	if (result) {
+		const tokenHeader = {
+			alg: 'HS256',
+			typ: 'JWT'
+		}
+		const tokenPayload = {
+			name: user,
+			role: 'user'
+		}
+		const encodedData = Buffer.from(JSON.stringify(tokenHeader)).toString('base64url') +
+			'.' +
+			Buffer.from(JSON.stringify(tokenPayload)).toString('base64url')
+
+		const signature = crypto.createHmac('sha256', secret)
+			.update(encodedData)
+			.digest('base64url')
+
+		const jwt = encodedData + '.' + signature
+		res.send(JSON.stringify({ jwt }))
+	} else {
+		res.send(500)
+	}
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+	console.log(`Example app listening on port ${port}`)
 })
